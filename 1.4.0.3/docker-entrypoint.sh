db88da9eae6836201e3e9174026cf33812d490cc
Juha Kovanen <juha.kovanen@gmail.com>
2016-09-08 22:44:44 +0300

    added docker-compose.yml

--- 1.4.0.3/docker-entrypoint.sh
+++ 1.4.0.3/docker-entrypoint.sh
@@ -24,24 +24,24 @@ then
 fi
 
 if [ $(echo "$1" | cut -c1) = "-" ]; then
-  echo "$0: assuming arguments for shadowcoind"
+    echo "$0: assuming arguments for shadowcoind"
 
-  set -- shadowcoind "$@"
+    set -- shadowcoind "$@"
 fi
 
 if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "shadowcoind" ]; then
-  mkdir -p "$SHADOWCASH_DATA"
-  chmod 700 "$SHADOWCASH_DATA"
-  chown -R shadowcash "$SHADOWCASH_DATA"
+    mkdir -p "$SHADOWCASH_DATA"
+    chmod 700 "$SHADOWCASH_DATA"
+    chown -R shadowcash "$SHADOWCASH_DATA"
 
-  echo "$0: setting data directory to $SHADOWCASH_DATA"
+    echo "$0: setting data directory to $SHADOWCASH_DATA"
 
-  set -- "$@" -datadir="$SHADOWCASH_DATA"
+    set -- "$@" -datadir="$SHADOWCASH_DATA"
 fi
 
 if [ "$1" = "shadowcoind" ]; then
-  echo
-  exec gosu shadowcash "$@"
+    echo
+    exec gosu shadowcash "$@"
 fi
 
 echo
